783b0bad0b3137570905fe18bdea763c8ba641c8
danifer24 <danielvf24@gmail.com>
2025-09-03 15:04:31 +0200

    Añadir react-router-dom y configurar rutas en CartApp para mejorar la navegación entre catálogo y carrito

--- src/CartApp.jsx
+++ src/CartApp.jsx
@@ -1,3 +1,4 @@
+import { Navigate, Route, Routes } from "react-router-dom";
 import { CartView } from "./components/CartView";
 import { CatalogView } from "./components/CatalogView";
 import { useItemsCart } from "./hooks/useItemsCart";
@@ -13,14 +14,29 @@ export const CartApp = () => {
             <div className="container my-4">
 
                 <h3>Cart App</h3>
-                <CatalogView handler={addToCart} />
+                <Routes>
+                    <Route
+                        path="catalog"
+                        element={<CatalogView handler={addToCart} />} />
+                    <Route
+                        path="cart"
+                        element={(
+                            cartItems?.length <= 0 ?
+                            <div className="alert alert-warning">No hay protucto en el carrito de compras!</div>
+                            :
+                            (
+                                <div className="my-4 w-50">
+                                    <CartView cartItems={cartItems} handlerDelete={deleteProduct} />
+                                </div>
+                            )
+                        )} />
 
-                {cartItems?.length <= 0 ||
-                    (
-                        <div className="my-4 w-50">
-                            <CartView cartItems={cartItems} handlerDelete={deleteProduct} />
-                        </div>
-                    )}
+                    <Route path="/" element={ <Navigate to={ '/catalog' } /> } />
+
+                </Routes>
+
+
+
 
             </div>
         </>
